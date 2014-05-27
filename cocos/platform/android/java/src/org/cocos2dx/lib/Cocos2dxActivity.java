a0b158c2eb03a8e79491c109422c854345942849
JulienM <julien.m@gameparkstudio.com>
2014-05-27 18:11:00 +0900

    patch stencil buffer activation. shift cocos2dx.iml from one directory (remove 2d).

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -183,8 +183,8 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
         mFrameLayout.addView(this.mGLSurfaceView);
 
         // Switch to supported OpenGL (ARGB888) mode on emulator
-        if (isAndroidEmulator())
-           this.mGLSurfaceView.setEGLConfigChooser(8 , 8, 8, 8, 16, 0);
+        //if (isAndroidEmulator())
+           this.mGLSurfaceView.setEGLConfigChooser(8 , 8, 8, 8, 16, 8);
 
         this.mGLSurfaceView.setCocos2dxRenderer(new Cocos2dxRenderer());
         this.mGLSurfaceView.setCocos2dxEditText(edittext);
